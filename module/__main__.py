52fc3aabc9f156ad0b97852b8062bc591445d986
Spirit-act <16631523+Spirit-act@users.noreply.github.com>
2021-12-15 16:45:48 +0100

    add root path detection

--- module/__main__.py
+++ module/__main__.py
@@ -1,2 +1,8 @@
+import os
+
+
+def root_path() -> str:
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 if __name__ == "__main__":
     print("module was executed in standalone")
